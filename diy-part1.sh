270ceebbb73ca31e4aac9a5cfa0bcbf71344c906
GarramZahard <portalforfuture@gmail.com>
2025-07-02 12:16:52 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,24 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# --- Tambahkan patch DTS agar port USB aktif di Xiaomi Mi Router 4C ---
+patch -p1 << 'EOF'
+--- a/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
++++ b/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
+@@
+ &ehci {
+-       status = "disabled";
++       status = "okay";
+ };
+
+ &ohci {
+-       status = "disabled";
++       status = "okay";
+ };
+EOF
