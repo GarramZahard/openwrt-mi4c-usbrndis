0d5080cb6e4d6ed75c2123e92f02614dd0259476
GarramZahard <portalforfuture@gmail.com>
2025-07-02 13:50:40 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,19 +8,3 @@
 # (Opsional) Ganti atau tambah feed source jika diperlukan
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# Tambahkan patch untuk mengaktifkan USB di Mi Router 4C
-patch -d openwrt -p1 << 'EOF'
---- a/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
-+++ b/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
-@@
- &ehci {
--       status = "disabled";
-+       status = "okay";
- };
-
- &ohci {
--       status = "disabled";
-+       status = "okay";
- };
-EOF
