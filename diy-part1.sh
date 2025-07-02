1ff69af0658a29f6adbfc68cd15d3729ed1ea3e0
GarramZahard <portalforfuture@gmail.com>
2025-07-02 13:16:57 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,5 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# Masuk ke direktori sumber OpenWrt
-cd openwrt || exit 1
-
 # Patch DTS agar USB aktif untuk Xiaomi Mi Router 4C
 patch -p1 < "$GITHUB_WORKSPACE/patches/0001-enable-usb.patch"
