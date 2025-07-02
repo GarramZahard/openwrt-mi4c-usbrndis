cf10194f8a9b2965157d1e1fc7956f77e10ce79d
GarramZahard <portalforfuture@gmail.com>
2025-07-02 12:34:54 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,19 +9,3 @@
 
 # Ubah hostname (opsional)
 sed -i 's/OpenWrt/OpenWrt-4C/g' package/base-files/files/bin/config_generate
-
-# Aktifkan EHCI & OHCI untuk USB agar RNDIS bisa dipakai
-patch -p1 <<'EOF'
---- a/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
-+++ b/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
-@@
- &ehci {
--       status = "disabled";
-+       status = "okay";
- };
-
- &ohci {
--       status = "disabled";
-+       status = "okay";
- };
-EOF
