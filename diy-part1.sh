daa4e27ed6270c7e507dd461c9670b02609a6198
GarramZahard <portalforfuture@gmail.com>
2025-07-02 13:10:37 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,28 +4,13 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Ganti sumber feed bila perlu
+# (Opsional) Ganti atau tambah feed source jika diperlukan
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# Masuk ke direktori sumber OpenWrt
+cd openwrt || exit 1
 
 # Patch DTS agar USB aktif untuk Xiaomi Mi Router 4C
 patch -p1 < "$GITHUB_WORKSPACE/patches/0001-enable-usb.patch"
